94013027b423e0a9c507968cad5fdec4ffffc9b4
peishan0824 <peishan0824@gmail.com>
2024-02-29 18:22:31 -0600

    revise R code

--- NMA_Depression_Linde2015_02282024.R
+++ NMA_Depression_Linde2015_02282024.R
@@ -1,47 +1,26 @@
-
-library(meta)
-library(netmeta)
+# library(meta)  # R package for pairwise meta analysis 
+######################################################################################################## Frequentist NMA
+######################################################################################################## Frequentist NMA
+######################################################################################################## Frequentist NMA
+library(netmeta) # R package for NMA
 data(Linde2015)
-trts<-c("TCA","SSRI","SNRI","NRI", "Low-dose SARI",
-        "NaSSa","rMAO-A","Hypericum","Placebo")
-outcomes<-c("Earlyresponse", "Earlyremission")
+trts <- c("TCA","SSRI","SNRI","NRI", "Low-dose SARI",
+          "NaSSa","rMAO-A","Hypericum","Placebo")
+View(Linde2015)
+# resp1/2/3	Number of early responder (treatment 1/2/3)
+# n1/2/3    Number of patients receiving treatment 1/2/3
 
+# Outcome: Early Response -> OR: Odds Ratio
+# Transform data from two arm-based formats into contrast-based format
+p1 <- pairwise(treat = list(treatment1,treatment2,treatment3), 
+               event = list(resp1,resp2,resp3),
+               n = list(n1,n2,n3), 
+               studlab = id,
+               data = Linde2015, sm = "OR") 
 
-
-#(1)Earlyresponse #
-p1<-pairwise(treat=list(treatment1,treatment2,treatment3), 
-             event=list(resp1,resp2,resp3),
-             n=list(n1,n2,n3), 
-             studlab=id,
-             data=Linde2015,sm="OR") 
-net1<-netmeta(p1,common=TRUE, seq=trts,ref ="Placebo",small="undesirable")
+net1 <- netmeta(p1, common = TRUE, seq = trts,
+                ref = "Placebo", small="undesirable")
 summary(net1, ref="Placebo")
-
-library(dplyr)
-data <- p1 %>% 
-  filter(treat1 =="TCA" | treat1 =="Hypericum" | treat1 =="NaSSa" ) %>%
-  filter(treat2 =="TCA" | treat2 =="Hypericum" | treat2 =="NaSSa" ) 
-net2<-netmeta(data,common=TRUE, seq=trts<-c("TCA","Hypericum","NaSSa"),
-              ref ="TCA",small="undesirable")
-summary(net2, ref="TCA")
-
-# 2b. Comparison of all treatment pairs
-pairs2 <- netleague(net2, digits = 2,seq=trts)
-View(pairs2$common)
-forest(net2, ref="TCA", xlab="OR", pooled="common")
-
-# network graph
-netgraph(net2, 
-         plastic = FALSE,
-         number.of.studies = TRUE,
-         multiarm = TRUE,
-         seq = "optimal",
-         cex = 1,
-         rotate = 0,
-         cex.points = n.trts,
-         labels = paste0(gsub("+", " +\n", trts, 
-                              fixed = TRUE),
-                         "\n(n=", round(n.trts), ")"))
 
 # Treatment Ranking
 # P-scores allow ranking the treatments on a continuous 0-1 scale
@@ -68,9 +47,37 @@ print(netsplit(net1), digits=2)
 # when the results of a direct comparison (i.e., a head-to-head comparison of two treatments) do not agree with
 #      the results of an indirect comparison (i.e., a comparison that is inferred through a common comparator).
 
-############################################################################################################ Bayesian NMA
-############################################################################################################ Bayesian NMA
-############################################################################################################ Bayesian NMA
+
+# Motivating Example: Estimate relative effective between H and N
+library(dplyr)
+data <- p1 %>% 
+  filter(treat1 =="TCA" | treat1 =="Hypericum" | treat1 =="NaSSa" ) %>%
+  filter(treat2 =="TCA" | treat2 =="Hypericum" | treat2 =="NaSSa" ) 
+net2 <- netmeta(data,common=TRUE, seq=trts<-c("TCA","Hypericum","NaSSa"),
+                ref ="TCA",small="undesirable")
+summary(net2, ref="TCA")
+
+# 2b. Comparison of all treatment pairs
+pairs2 <- netleague(net2, digits = 2,seq=trts)
+View(pairs2$common)
+forest(net2, ref="TCA", xlab="OR", pooled="common")
+
+# network graph
+netgraph(net2, 
+         plastic = FALSE,
+         number.of.studies = TRUE,
+         multiarm = TRUE,
+         seq = "optimal",
+         cex = 1,
+         rotate = 0,
+         cex.points = n.trts,
+         labels = paste0(gsub("+", " +\n", trts, 
+                              fixed = TRUE),
+                         "\n(n=", round(n.trts), ")"))
+
+######################################################################################################## Bayesian NMA
+######################################################################################################## Bayesian NMA
+######################################################################################################## Bayesian NMA
 # install JAGS 4.3.0 for R version 4.1.2
 # install.packages("https://cran.r-project.org/src/contrib/Archive/rjags/rjags_4-10.tar.gz", type="Source", repos=NULL)
 # Self-learning material:
@@ -81,7 +88,8 @@ library(rjags)
 library(gemtc)
 
 # network setup: data with long arm-based format
-NMA_Baye <- mtc.network(data.ab = data.long.arm.base)
+data_f <- read.csv(file = "longformat_for_R_Package_gemtc.csv" , header = TRUE)
+NMA_Baye <- mtc.network(data.ab = data_f)
 summary(NMA_Baye)
 plot(NMA_Baye)
 # network model - fixed effect model
@@ -98,7 +106,7 @@ ranks <- rank.probability(mcmc_Baye)
 print(sucra(ranks))
 
 # 2a. Forest Plot
-forest(relative.effect(mcmc_Baye, t1="Sham"), digits=4)
+forest(relative.effect(mcmc_Baye, t1="Placebo"), digits=4)
 
 # 2b. Comparison of all treatment pairs
 round(relative.effect.table(mcmc_Baye, covariate=NA), 2)
